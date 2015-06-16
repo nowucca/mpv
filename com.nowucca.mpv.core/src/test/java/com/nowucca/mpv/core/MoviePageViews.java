40c06dd1f5258a33b3b138d2a554cc7d0ac4dffa
Steven Atkinson <steven@atkinson.net>
2015-06-16 02:15:38 -0700

    re-implemented to be closer to a reactive streams implementation but growing the movie object seems cheesy

--- com.nowucca.mpv.core/src/test/java/com/nowucca/mpv/core/MoviePageViews.java
+++ com.nowucca.mpv.core/src/test/java/com/nowucca/mpv/core/MoviePageViews.java
@@ -4,6 +4,7 @@ import com.nowucca.mpv.util.UTF8;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.node.ObjectNode;
@@ -21,6 +23,9 @@ import org.jdom2.Namespace;
 import org.jdom2.input.SAXBuilder;
 import rx.Observable;
 import rx.Observer;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.schedulers.Schedulers;
 
 /**
  * Rank HBO GO available movies by popularity based on matching Wikipedia page views.
@@ -37,12 +42,81 @@ public class MoviePageViews {
     public static final String HBO_GO_MOVIE_LIST_URL = "http://xfinitytv.comcast.net/movie.widget";
     public static final String WIKIPEDIA_PAGE_URL_PREFIX = "http://stats.grok.se/json/en/201408/";
 
+    private static final List<Movie> rankedMovies = new ArrayList<Movie>(1000);
+
     /**
      * Get a list of movies and rank them by page view using JSON output.
      */
     public static void main(String[] args) throws Exception {
         Observable<Movie> movies = getMovies();
-        movies.take(50).subscribe(new MovieObserver());
+        movies.map((m) -> setWikiUrl(m))
+                .filter((m)->m.getWikiUrl() != null)
+                .map((m) -> setJSON(m, 10000))
+                .filter((m) -> m.getJSON() != null)
+                .map((m) -> setTotalPageViews(m))
+                .filter((m)->(m.getTotalPageViews() != null))
+                .take(50)
+                .subscribe(new Subscriber<Movie>() {
+                    @Override
+                    public void onError(Throwable throwable) {
+                        throwable.printStackTrace();
+                        System.out.println("Encountered error.");
+                    }
+
+                    @Override
+                    public void onNext(Movie movie) {
+                        rankedMovies.add(movie);
+                        System.out.println("Processed " + movie.getTitle());
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                        System.out.println("Completed.");
+
+                        Collections.sort(rankedMovies);
+                        final ObjectMapper mapper = new ObjectMapper();
+                        final ObjectNode root = mapper.createObjectNode();
+
+                        Observable.from(rankedMovies)
+                                .doOnNext((movie) -> {
+                                    ObjectNode movieNode = root.putObject("movie-" + movie.getId());
+                                    movieNode.put("title", movie.getTitle());
+                                    movieNode.put("id", movie.getId());
+                                    movieNode.put("totalPageViews", movie.getTotalPageViews());
+                                })
+                                .subscribe(new Subscriber<Movie>() {
+                                    @Override
+                                    public void onCompleted() {
+                                        try {
+                                            mapper.writeValue(System.out, root);
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                            throw new RuntimeException(e);
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onError(Throwable throwable) {
+
+                                    }
+
+                                    @Override
+                                    public void onNext(Movie movie) {
+
+                                    }
+                                });
+                    }
+                });
+    }
+
+    private static Movie setWikiUrl(Movie m) {
+        try {
+            m.setWikiUrl(WIKIPEDIA_PAGE_URL_PREFIX + URLEncoder.encode(m.title, UTF8.asString()));
+        } catch (UnsupportedEncodingException e) {
+            m.setWikiUrl(null);
+        }
+        return m;
+
     }
 
 
@@ -64,70 +138,38 @@ public class MoviePageViews {
         return Observable.from(movieList);
     }
 
-    /**
-     * Obtains page ranks for each observed movie, and ranks all movies by totalPageViews.
-     */
-    private static class MovieObserver implements Observer<Movie> {
-        private final List<Movie> rankedMovies = new ArrayList<Movie>(1000);
-
-        @Override
-        public void onNext(Movie movie) {
-            System.out.print("Processing movie \"" + movie.title);
-
-            // Obtain page view data and sum it from the network source.
-            try {
-                String json = getJSON(WIKIPEDIA_PAGE_URL_PREFIX + URLEncoder.encode(movie.title, UTF8.asString()), 10000);
-                if (json != null) {
-                    ObjectMapper m = new ObjectMapper();
-                    JsonNode rootNode = m.readTree(json);
-                    long totalPageViews = 0;
-                    for (Iterator<JsonNode> it = rootNode.path("daily_views").getElements(); it.hasNext(); ) {
-                        int dailyPageView = it.next().asInt(-100);
-                        if (dailyPageView != -100) {
-                            totalPageViews += dailyPageView;
-                        }
-                    }
-                    movie.setTotalPageViews(totalPageViews);
-                    rankedMovies.add(movie);
-                    System.out.println("\" totalPageViews= "+totalPageViews);
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-        @Override
-        public void onCompleted() {
-            Collections.sort(rankedMovies);
+    static Movie setTotalPageViews(Movie movie)  {
+        if (movie.getJSON() != null) {
             ObjectMapper m = new ObjectMapper();
-            ObjectNode root = m.createObjectNode();
-
-            for (Movie movie : rankedMovies) {
-                ObjectNode movieNode = root.putObject("movie-" + movie.getId());
-                movieNode.put("title", movie.getTitle());
-                movieNode.put("id", movie.getId());
-                movieNode.put("totalPageViews", movie.getTotalPageViews());
-            }
-
+            JsonNode rootNode = null;
             try {
-                m.writeValue(System.out, root);
+                rootNode = m.readTree(movie.getJSON());
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
+                movie.setTotalPageViews(null);
+                return movie;
             }
+            long totalPageViews = 0;
+            for (Iterator<JsonNode> it = rootNode.path("daily_views").getElements(); it.hasNext(); ) {
+                int dailyPageView = it.next().asInt(-100);
+                if (dailyPageView != -100) {
+                    totalPageViews += dailyPageView;
+                }
+            }
+            movie.setTotalPageViews(totalPageViews);
         }
 
-        @Override
-        public void onError(Throwable e) {
-            System.out.println("Received error during movie processing: " + e);
-        }
+        return movie;
     }
 
     /**
      * An expensive synchronous blocking call to read a JSON string from a URL.
+     *
      * @return {@code null} when there is a problem accessing the JSON data.
      */
-    public static String getJSON(String url, int timeout) {
+    public static Movie setJSON(Movie movie, int timeout) {
         try {
-            URL u = new URL(url);
+            URL u = new URL(movie.getWikiUrl());
             HttpURLConnection c = (HttpURLConnection) u.openConnection();
             c.setRequestMethod("GET");
             c.setRequestProperty("Content-length", "0");
@@ -148,13 +190,16 @@ public class MoviePageViews {
                         sb.append(line).append("\n");
                     }
                     br.close();
-                    return sb.toString();
+                    movie.setJSON(sb.toString());
+                    break;
+                default:
+                    movie.setJSON(null);
             }
 
         } catch (Exception ex) {
-            return null;
+            movie.setJSON(null);
         }
-        return null;
+        return movie;
     }
 
     /**
@@ -168,7 +213,9 @@ public class MoviePageViews {
         String title; // data-t
         String href; // href;
 
-        long totalPageViews;
+        Long totalPageViews;
+        private String wikiUrl;
+        private String JSON;
 
         public Movie(String id, String title, String href) {
             this.id = id;
@@ -176,18 +223,18 @@ public class MoviePageViews {
             this.href = href;
         }
 
-        public void setTotalPageViews(long totalPageViews) { this.totalPageViews = totalPageViews; }
+        public void setTotalPageViews(Long totalPageViews) { this.totalPageViews = totalPageViews; }
 
         public String getId() { return id; }
         public String getTitle() { return title; }
-        public long getTotalPageViews() { return totalPageViews; }
+        public Long getTotalPageViews() { return totalPageViews; }
 
         public int compare(Movie o1, Movie o2) {
             if (o1==o2) { return 0; }
             if (o1 != null && o2 == null) { return -1; }
             if (o1 == null) {return 1;}
             if (o1.totalPageViews != o2.totalPageViews) {
-                return (int) (o1.totalPageViews - o2.totalPageViews);
+                return (int) (o2.totalPageViews - o1.totalPageViews);
             }
             return o1.title.compareTo(o2.title);
         }
@@ -196,6 +243,25 @@ public class MoviePageViews {
         public int compareTo(Movie movie) {
             return compare(this, movie);
         }
+
+        public void setWikiUrl(String wikiUrl) {
+            this.wikiUrl = wikiUrl;
+        }
+
+        public String getWikiUrl() {
+            return wikiUrl;
+        }
+
+        public void setJSON(String JSON) {
+            this.JSON = JSON;
+        }
+
+        public String getJSON() {
+            return JSON;
+        }
+
+
+
     }
 }
 
